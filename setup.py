dfa44fa675919455b4d5a7be60be64c7e6f7809c
Takayuki Kaisen <ksn0215@gmail.com>
2017-05-08 02:00:15 +0900

    added seutp.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='sampling',
+      version='0.1',
+      description='Bootstrapping and Delta method',
+      url='https://github.com/ksnt/Bootstrap-and-Deltamethod',
+      author='Takayuki Kaisen',
+      author_email='ksn0215@gmail.com',
+      license='MIT',
+      packages=['sampling'],
+      zip_safe=False)
