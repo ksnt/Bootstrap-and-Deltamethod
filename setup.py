a141972053fddeae31b01c6699b09d1da20d3b20
Takayuki Kaisen <ksn0215@gmail.com>
2017-05-08 02:33:14 +0900

    a

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup,find_packages
 
 setup(name='sampling',
       version='0.1',
@@ -7,5 +7,7 @@ setup(name='sampling',
       author='Takayuki Kaisen',
       author_email='ksn0215@gmail.com',
       license='MIT',
-      packages=['sampling'],
-      zip_safe=False)
+      packages=find_packages(),
+      zip_safe=False
+       
+)
